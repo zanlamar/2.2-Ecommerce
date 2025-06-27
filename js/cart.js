05c577815144a23fc3ae8f6602bb1562f16ee1ab
Ana Laf <zanlamar@gmail.com>
2025-06-27 18:12:35 +0200

    refactorization WIP: checkout totals ready

---
+++ js/cart.js
@@ -0,0 +1,95 @@
+"use strict"
+
+// las constantes que van de uno a otro
+import { products } from './products.js'
+
+export const cart = [];
+ 
+// update the product cart counter at navbar
+export const updateCounter = () => {
+    const counter = document.getElementById("count_product");
+    const totalItems = cart.reduce((sum, product) => sum + product.quantity, 0);
+    counter.textContent = totalItems;
+    return totalItems;
+} ;
+
+export const buy = (id) => {
+    // 1. Loop for to the array products to get the item to add to cart
+    let item = products.find(product => product.id === id);
+    let cartItem = cart.find(product => product.id === id);
+    let newCartItem = (!cartItem)? true: false;
+
+    // 2. Add found product to the cart array
+    if (newCartItem) {cart.push({...item, quantity: 1});
+        } else {
+        cartItem.quantity++;
+    }; 
+    updateCounter();
+    // printCart();
+    console.log('buy', id, cart);
+};
+
+export const calculatePriceSingle = (product) => {
+    let total = product.price * product.quantity;
+    console.log('calculatesingle', total.toFixed(2)); // este si funciona
+    return total.toFixed(2); 
+}
+
+export const applyPromotionSingle = ((product) => {
+    let finalDiscountedPrice = 0;
+
+    if (product.offer && product.quantity >= product.offer.number) {
+            const discount = product.price * (product.offer.percent / 100);
+            const promoPrice = product.price - discount;
+            return promoPrice * product.quantity;
+    } else {
+        finalDiscountedPrice = calculatePriceSingle(product);
+        console.log('promotionsingleCart', finalDiscountedPrice)
+        return finalDiscountedPrice;
+    };
+});
+
+// esta se usa realmente? yo creo que no
+export const calculateTotal = () =>  {
+    let total = 0;
+    cart.forEach(element => { 
+        // total += element.price * element.quantity}); 
+        calculatePriceSingle(element)});
+    return total.toFixed(2);
+};
+
+export const applyPromotionsCart = () =>  {
+    let subtotalWithDiscount = 0;
+
+    cart.forEach((element) => {
+        if (element.offer && element.quantity >= element.offer.number) {
+            const discount = element.price * (element.offer.percent / 100);
+            const promoPrice = element.price - discount; // esto es interesante que esté en una nueva variable porque es muy goloso para marketing
+            subtotalWithDiscount += promoPrice * element.quantity;
+        } else {
+            subtotalWithDiscount += calculatePriceSingle(element);
+        };
+    });
+};
+
+
+export const removeFromCart = (id) => {
+    const index = cart.findIndex(p => p.id === id);
+
+    if (index !== -1) {
+        cart.splice(index, 1);
+        printCart();
+        updateCounter();
+    };
+};
+
+export const cleanCart = () =>  {
+    cart.splice(0, cart.length); // this way it gets empty
+    updatePage("count_product", 0); // this way it shows that is empty
+    printCart(); // cleans the view of the cart
+};
+
+
+
+
+
--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,18 @@
+"use strict"
 import { products } from './products.js';
+
+import {
+    cart,
+    updateCounter,
+    buy,
+    calculatePriceSingle,
+    applyPromotionSingle,
+    calculateTotal,
+    applyPromotionsCart,
+    removeFromCart,
+    cleanCart,
+} from './cart.js';
+
 
 // => Reminder, it's extremely important that you debug your code. 
 // ** It will save you a lot of time and frustration!
@@ -6,9 +20,9 @@ import { products } from './products.js';
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-const cart = [];
+// const cart = [];
 
-// change DOM by ID
+// change DOM by ID - TBC
 function updatePage(id, newValue){
     const element = document.getElementById(id);
     if (element) element.innerHTML = newValue;
@@ -21,97 +35,97 @@ document.addEventListener('DOMContentLoaded', () => {
             button.addEventListener('click', event => {
                 const id = button.getAttribute('data-product-id');
                 buy(parseInt(id));
+                printCart();
             });
     });
 });
 
-    // update the product cart counter at navbar
-    const updateCounter = () => {
-        const counter = document.getElementById("count_product");
-        const totalItems = cart.reduce((sum, product) => sum + product.quantity, 0);
-        counter.textContent = totalItems;
-    };
+    // // update the product cart counter at navbar
+    // const updateCounter = () => {
+    //     const counter = document.getElementById("count_product");
+    //     const totalItems = cart.reduce((sum, product) => sum + product.quantity, 0);
+    //     counter.textContent = totalItems;
+    //     return totalItems;
+    // } 
 
 // Exercise 1
-const buy = (id) => {
-    console.log(`producto ID ${id}`);
+// const buy = (id) => {
+//     console.log(`producto ID ${id}`);
 
-    // 1. Loop for to the array products to get the item to add to cart
-    let item = products.find(product => product.id === id);
-    let cartItem = cart.find(product => product.id === id);
-    let newCartItem = (!cartItem)? true: false;
+//     // 1. Loop for to the array products to get the item to add to cart
+//     let item = products.find(product => product.id === id);
+//     let cartItem = cart.find(product => product.id === id);
+//     let newCartItem = (!cartItem)? true: false;
 
-    // 2. Add found product to the cart array
-    if (newCartItem) {cart.push({...item, quantity: 1});
-        } else {
-        cartItem.quantity++;
-    };
+//     // 2. Add found product to the cart array
+//     if (newCartItem) {cart.push({...item, quantity: 1});
+//         } else {
+//         cartItem.quantity++;
+//     };
 
-    updateCounter();
-    printCart();
-}
-// Exercise 2 - empty cart (how to include this at the beggining?)
-const cleanCart = () =>  {
-    cart.splice(0, cart.length); // this way it gets empty
-    updatePage("count_product", 0); // this way it shows that is empty
-    printCart(); // cleans the view of the cart
-};
+//     updateCounter();
+//     printCart();
+// }
+// // Exercise 2 - empty cart (how to include this at the beggining?)
+// const cleanCart = () =>  {
+//     cart.splice(0, cart.length); // this way it gets empty
+//     updatePage("count_product", 0); // this way it shows that is empty
+//     printCart(); // cleans the view of the cart
+// };
 
 const emptyCart = document.getElementById("clean-cart");
-console.log(emptyCart); 
 emptyCart.addEventListener('click', cleanCart);
+// console.log(emptyCart); 
 
 // Exercise 3
 // Calculate total price of the cart using the "cartList" array
-const calculateTotal = () =>  {
-    let total = 0;
-    cart.forEach(element => { 
-        total += element.price * element.quantity}); 
-    return total.toFixed(2);
-}
+// const calculateTotal = () =>  {
+//     let total = 0;
+//     cart.forEach(element => { 
+//         total += element.price * element.quantity}); 
+//     return total.toFixed(2);
+// }
 
 // Exercise 4
 // Apply promotions to each item in the array "cart"
-const applyPromotionsCart = () =>  {
-    let subtotalWithDiscount = 0;
+// const applyPromotionsCart = () =>  {
+//     let subtotalWithDiscount = 0;
 
-    cart.forEach((element) => {
+//     cart.forEach((element) => {
 
-        if (element.offer && element.quantity >= element.offer.number) {
-            const discount = element.price * (element.offer.percent / 100);
-            const promoPrice = element.price - discount;
-            subtotalWithDiscount += promoPrice * element.quantity;
-        } else {
-            subtotalWithDiscount += element.price * element.quantity;
-        };
+//         if (element.offer && element.quantity >= element.offer.number) {
+//             const discount = element.price * (element.offer.percent / 100);
+//             const promoPrice = element.price - discount;
+//             subtotalWithDiscount += promoPrice * element.quantity;
+//         } else {
+//             subtotalWithDiscount += element.price * element.quantity;
+//         };
 
-    }); return subtotalWithDiscount.toFixed(2);
-};
+//     }); return subtotalWithDiscount.toFixed(2);
+// };
 
 
 // apply products to a single product
+// creo que esta funcion no se usa realmente para nada?
+// const applyPromotionSingle = ((product) => {
+//     let finalDiscountedPrice = 0;
 
-const applyPromotionSingle = ((product) => {
-    let finalDiscountedPrice = 0;
-
-    if (product.offer && product.quantity >= product.offer.number) {
-            const discount = product.price * (product.offer.percent / 100);
-            const promoPrice = product.price - discount;
-            finalDiscountedPrice = promoPrice * product.quantity;
-    } else {
-        finalDiscountedPrice = product.price * product.quantity;
-    };
-    return finalDiscountedPrice.toFixed(2); 
-});
+//     if (product.offer && product.quantity >= product.offer.number) {
+//             const discount = product.price * (product.offer.percent / 100);
+//             const promoPrice = product.price - discount;
+//             finalDiscountedPrice = promoPrice * product.quantity;
+//     } else {
+//         finalDiscountedPrice = product.price * product.quantity;
+//     };
+//     return finalDiscountedPrice.toFixed(2); 
+// });
 
 // Exercise 5
 // Fill the shopping cart modal manipulating the shopping cart dom
 
 const printCart = () => {
     const cartList = document.getElementById("cart_list"); // identify the elements in order to change them later
-    const totalPrice = document.getElementById("total_price");
     const container = document.getElementById("total_container");
-    const table = document.querySelector(".table");
     const CTAs = document.getElementById("checkout-buttons");
     
     cartList.innerHTML = '';
@@ -124,13 +138,13 @@ const printCart = () => {
             </tr>
             `;
         container.style.display = "none"; // to hide the total span, para que no aparezca el total
-        CTAs.style.display = "none";
+        CTAs.style.display = "none"; // para que no aparezcan los botones
         return;
 
     } else { 
         container.style.display = 'block';
         CTAs.style.display = "block";
-    }
+    };
 
 
     cart.forEach((element) => {
@@ -208,15 +222,15 @@ document.addEventListener('DOMContentLoaded', () => {
 // ** Nivell II **
 
 // Exercise 7
-const removeFromCart = (id) => {
-    const index = cart.findIndex(p => p.id === id);
+// const removeFromCart = (id) => {
+//     const index = cart.findIndex(p => p.id === id);
 
-    if (index !== -1) {
-        cart.splice(index, 1);
-        printCart();
-        updateCounter();
-    };
-};
+//     if (index !== -1) {
+//         cart.splice(index, 1);
+//         printCart();
+//         updateCounter();
+//     };
+// };
 
 
 const open_modal = () =>  {
