05e577f4cd0886fc39592bfc53ae5fed82af7ae9
Ana Laf <zanlamar@gmail.com>
2025-06-28 19:19:51 +0200

    cart.js no comments no console.log

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,9 @@
 "use strict"
 
-// las constantes que van de uno a otro
 import { products } from './products.js'
 
 export const cart = [];
  
-// update the product cart counter at navbar
 export const updateCounter = () => {
     const counter = document.getElementById("count_product");
     const totalItems = cart.reduce((sum, product) => sum + product.quantity, 0);
@@ -14,12 +12,10 @@ export const updateCounter = () => {
 } ;
 
 export const buy = (id) => {
-    // 1. Loop for to the array products to get the item to add to cart
     let item = products.find(product => product.id === id);
     let cartItem = cart.find(product => product.id === id);
     let newCartItem = (!cartItem)? true: false;
 
-    // 2. Add found product to the cart array
     if (newCartItem) {cart.push({...item, quantity: 1});
         } else {
         cartItem.quantity++;
@@ -31,7 +27,6 @@ export const buy = (id) => {
 
 export const calculatePriceSingle = (product) => {
     let total = product.price * product.quantity;
-    console.log('calculatesingle', total.toFixed(2)); // este si funciona
     return total; 
 }
 
@@ -44,12 +39,10 @@ export const applyPromotionSingle = ((product) => {
             return promoPrice * product.quantity;
     } else {
         finalDiscountedPrice = calculatePriceSingle(product);
-        console.log('promotionsingleCart', finalDiscountedPrice) // ahora si, YAS!
         return finalDiscountedPrice;
     };
 });
 
-// esta se usa realmente? yo creo que no
 export const calculateTotal = () =>  {
     let total = 0;
     cart.forEach(element => { 
@@ -64,22 +57,19 @@ export const applyPromotionsCart = () =>  {
     cart.forEach((element) => {
         if (element.offer && element.quantity >= element.offer.number) {
             const discount = element.price * (element.offer.percent / 100);
-            const promoPrice = element.price - discount; // esto es interesante que esté en una nueva variable porque es muy goloso para marketing
+            const promoPrice = element.price - discount; 
             subtotalWithDiscount += promoPrice * element.quantity;
-            console.log('applypromotionscart CON DESCUENTO', subtotalWithDiscount); // creo que es ok pero faltaria el tofixed
+            console.log('applypromotionscart CON DESCUENTO', subtotalWithDiscount); 
         } else {
             subtotalWithDiscount += calculatePriceSingle(element);
-            console.log('applypromotionscart sin DESCUENTO', subtotalWithDiscount); // creo que es ok pero faltaria el tofixed
+            console.log('applypromotionscart sin DESCUENTO', subtotalWithDiscount); 
         };
     });
     return subtotalWithDiscount.toFixed(2);
 };
 
 export const cleanCart = () =>  {
-    cart.splice(0, cart.length); // this way it gets empty
-    // ESTAS DOS COSAS LAS HARÁ MÁS ADELANTE, EN EL SHOP
-    // updatePage("count_product", 0); // this way it shows that is empty
-    // printCart(); // cleans the view of the cart
+    cart.splice(0, cart.length);
 };
 
 export const removeFromCart = (id) => {
